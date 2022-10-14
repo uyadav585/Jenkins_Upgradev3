1c8dd67091dfcb0c3e0694d0c7c1d805c7e39212
uyadav585 <uyadav585@gmail.com>
2022-10-14 21:08:30 +0530

    changes in test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,4 +40,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "Hi my name is umang";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
